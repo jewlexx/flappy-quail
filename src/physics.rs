814b563d74d0425a4859e4e01b91b628c65fd74d
Juliette Cordor <juliette@jewelexx.com>
2024-01-04 14:41:25 +1100

    ugh

--- src/physics.rs
+++ src/physics.rs
@@ -1,12 +1,22 @@
 use bevy::{prelude::*, sprite::collide_aabb::collide};
 
-use crate::{bird::Bird, pipes::PipePart, scoring::Scoreboard};
+use crate::{
+    bird::Bird,
+    pipes::{PipePart, PipeSection},
+    scoring::Scoreboard,
+};
 
-#[derive(Component)]
+#[derive(Default, Component)]
 pub struct Collider;
 
 #[derive(Default, Component, Deref, DerefMut, PartialEq)]
 pub struct Velocity(Vec2);
+
+impl Velocity {
+    pub const fn new(x: f32, y: f32) -> Self {
+        Self(Vec2::new(x, y))
+    }
+}
 
 #[derive(Event, Default)]
 pub struct CollisionEvent;
@@ -15,14 +25,14 @@ pub fn check_for_collisions(
     mut commands: Commands,
     mut scoreboard: ResMut<Scoreboard>,
     mut bird_query: Query<(Entity, &mut Velocity, &Transform), With<Bird>>,
-    collider_query: Query<(&Transform, &PipePart), With<Collider>>,
+    collider_query: Query<(&Transform, &PipeSection), With<Collider>>,
     mut collision_events: EventWriter<CollisionEvent>,
 ) {
     let (entity, mut _bird_velocity, bird_transform) = bird_query.single_mut();
     let ball_size = bird_transform.scale.truncate();
 
     // check collision with walls
-    for (pipe_transform, pipe_part) in &collider_query {
+    for (pipe_transform, pipe_section) in &collider_query {
         let collision = collide(
             bird_transform.translation,
             ball_size,
@@ -33,8 +43,8 @@ pub fn check_for_collisions(
             // Sends a collision event so that other systems can react to the collision
             collision_events.send_default();
 
-            match pipe_part.section() {
-                crate::pipes::PipeSection::Middle => scoreboard.score += 1,
+            match pipe_section {
+                PipeSection::Middle => scoreboard.score += 1,
                 _ => Bird::kill_bird(&mut commands, entity),
             }
 
--- src/pipes.rs
+++ src/pipes.rs
@@ -2,20 +2,63 @@ use bevy::prelude::*;
 
 use crate::physics::{Collider, Velocity};
 
-#[derive(Component)]
+#[derive(Bundle)]
 pub struct Pipes {
     top: PipePart,
     bottom: PipePart,
     middle: PipePart,
-    transform: Transform,
-    velocity: Velocity,
 }
 
-#[derive(Component)]
+impl Pipes {
+    pub const VELOCITY: Velocity = Velocity::new(-100.0, 0.0);
+
+    pub fn new(mut commands: Commands, asset_server: Res<AssetServer>) -> Self {
+        Self {
+            top: PipePart::new(asset_server.as_ref(), PipeSection::Top),
+            bottom: PipePart::new(asset_server.as_ref(), PipeSection::Bottom),
+            middle: PipePart::new(asset_server.as_ref(), PipeSection::Middle),
+        }
+    }
+}
+
+#[derive(Bundle)]
 pub struct PipePart {
-    sprite: Sprite,
+    sprite: SpriteBundle,
     collider: Collider,
+    transform: Transform,
     section: PipeSection,
+}
+
+impl PipePart {
+    const OFFSET: f32 = 0.0;
+
+    pub fn new(asset_server: &AssetServer, section: PipeSection) -> Self {
+        let sprite = match section {
+            PipeSection::Top => SpriteBundle {
+                texture: asset_server.load("pipe_top.png"),
+                ..Default::default()
+            },
+            PipeSection::Middle => todo!(),
+            PipeSection::Bottom => todo!(),
+        };
+
+        // let transform = match section {
+        //     // PipeSection::Top => Transform::from_translation(Vec3::new(
+        //     //     0.0,
+        //     //     sprite.sprite..y / 2.0 + Self::OFFSET,
+        //     //     0.0,
+        //     // )),
+        //     // PipeSection::Middle => todo!(),
+        //     // PipeSection::Bottom => todo!(),
+        // };
+
+        Self {
+            sprite,
+            collider: Collider,
+            transform: todo!(),
+            section,
+        }
+    }
 }
 
 impl PipePart {
@@ -24,7 +67,7 @@ impl PipePart {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Component, Clone, Copy, PartialEq, Eq)]
 pub enum PipeSection {
     Top,
     Middle,
